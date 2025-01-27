ecb546b957e6823e0891b58617ac19d41394200b
Jafer Hussein <123theelite@gmail.com>
2025-01-27 07:48:46 -0800

    adding comments to document module

--- server/modules/admin/controllers/role.controller.js
+++ server/modules/admin/controllers/role.controller.js
@@ -1,24 +1,35 @@
 const asyncHandler = require("express-async-handler");
-
 const Role = require("../models/role");
 
+//@desc get all roles
+//@route GET /api/v1/roles
+//@access admin only
 const getRoles = asyncHandler(async (req, res) => {
   const roles = await Role.find();
   res.status(200).json(roles);
 });
 
+//@desc get single role
+//@route GET /api/v1/roles/:id
+//@access admin only
 const getRole = asyncHandler(async (req, res) => {
   const { id } = req.params;
   const role = await Role.findById(id);
   res.status(200).json(role);
 });
 
+//@desc create role
+//@route POST /api/v1/roles
+//@access admin only
 const createRole = asyncHandler(async (req, res) => {
   const { name, description, access } = req.body;
   const role = await Role.create({ name, description, access });
   res.status(201).json(role);
 });
 
+//@desc update role
+//@route PUT /api/v1/roles/:id
+//@access admin only
 const updateRole = asyncHandler(async (req, res) => {
   const { id } = req.params;
   const { name, description, access } = req.body;
@@ -30,6 +41,9 @@ const updateRole = asyncHandler(async (req, res) => {
   res.status(200).json(role);
 });
 
+//@desc delete role
+//@route DELETE /api/v1/roles/:id
+//@access admin only
 const deleteRole = asyncHandler(async (req, res) => {
   const { id } = req.params;
   const role = await Role.findByIdAndDelete(id);
--- server/modules/document/controllers/document.controller.js
+++ server/modules/document/controllers/document.controller.js
@@ -4,26 +4,35 @@ const {
   default: { OK, CREATED },
 } = require("../../../constants/statuscode.names");
 
-//@desc get all accounts
-//@route
-//@access restricted
+//@desc get all document
+//@route GET /api/v1/document
+//@access admin only
 const getDocuments = asyncHandler(async (req, res) => {
   const documents = await Document.find();
   res.status(OK).json(documents);
 });
 
+//@desc get single document
+//@route GET /api/v1/document/:id
+//@access admin only
 const getDocument = asyncHandler(async (req, res) => {
   const { id } = req.params;
   const document = await Document.findById(id);
   res.status(OK).json(document);
 });
 
+//@desc create document
+//@route POST /api/v1/document
+//@access admin only
 const createDocument = asyncHandler(async (req, res) => {
   const { title, description, approval } = req.body;
   const document = await Document.create({ title, description, approval });
   res.status(CREATED).json(document);
 });
 
+//@desc update document
+//@route PUT /api/v1/document/:id
+//@access admin only
 const updateDocument = asyncHandler(async (req, res) => {
   const { id, title, description, approval } = req.body;
   const document = await Document.findByIdAndUpdate(
@@ -34,6 +43,9 @@ const updateDocument = asyncHandler(async (req, res) => {
   res.status(OK).json(document);
 });
 
+//@desc delete document
+//@route DELETE /api/v1/document/:id
+//@access admin only
 const deleteDocument = asyncHandler(async (req, res) => {
   const { id } = req.params;
   const document = await Document.findByIdAndDelete(id);
